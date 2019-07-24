6c33f63597b7054f66dfb71aafd98f4f6be1400b
root <root@cs42341>
2019-07-24 09:12:42 +0300

    fix

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 	# I'm sorry, Damir, but I needed to see it, so I made a small patch for myself
 	#if (file_exists($injection = __DIR__.'/medicaments/index.php')) {require_once $injection;return;}
-
+	if(!isset($_GET['nr'])){
+		if ((!isset($_SERVER['HTTP_IS_HTTPS']) || $_SERVER['HTTP_IS_HTTPS'] !== 'on') && $_SERVER['REQUEST_URI'] !== '/robots.txt' && (!isset($_SERVER['IS_HTTPS']) || $_SERVER['IS_HTTPS'] !== 'on')) {
+			$redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+			header('HTTP/1.1 301 Moved Permanently');
+			header("Location: $redirect");
+			exit;
+		}
+	}
 	include_once ('config.php');
 
 	$start_time = microtime(true);
