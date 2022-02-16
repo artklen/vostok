ca50bf8a34913e6f3204e7ff8e2177c5b1a32399
targrik <mail@artklen.ru>
2022-02-16 10:25:16 +0300

    тестирование изменений api оплаты

--- app/aquiring/test_routes.php
+++ app/aquiring/test_routes.php
@@ -1,5 +1,7 @@
 <?php
 
 d()->route('/test_payment', static function() {
-    d()->emit('aquiring.successfull_paid', [d()->Order->f(2)]);
+    if (iam('developer')) {
+        d()->emit('aquiring.successfull_paid', [d()->Order->f(2)]);
+    }
 });
