5994eb640ac81df53e0c529ff167d91b8625787b
targrik <mail@artklen.ru>
2023-01-13 16:13:36 +0300

    1

--- app/admin/orders_edit.php
+++ app/admin/orders_edit.php
@@ -6,12 +6,17 @@ d()->post('/admin/edit/orders/:key', function ($id){
         if (d()->order->status_id != d()->params['status_id']){
             $order = d()->Order->f($id);
             $order->status_id = d()->params['status_id'];
+            $order->track = d()->params['track'];
             $order->save();
             $user = d()->User->find_by('id', d()->order->user_id);
             $order = d()->Order($id);
             if (d()->order->user_id != "" && $user->ne) {
                 d()->notification->notice_user_order_status_change( $order);
             }
+        } else {
+            $order = d()->Order->f($id);
+            $order->track = d()->params['track'];
+            $order->save();
         }
     }
     header('Location: /admin/list/orders');
